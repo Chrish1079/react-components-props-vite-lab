e68cfd8c899cd887908889b9e1f7e1353b3eaaf7
Chrish1079 <itssuper1079@gmail.com>
2025-11-04 20:20:00 -0500

    submit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+function About({ image = "https://via.placeholder.com/215", about }) {
+  return (
+    <>
+      <aside>
+        <img src={image} alt="blog logo" />
+      </aside>
+      <p>{about}</p>
+    </>
+  );
+}
+
+export default About;
+
